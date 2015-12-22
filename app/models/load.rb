eb447069b06564b361eabae7918403872c0a4deb
Dan Bunker <dan@Dans-MacBook-Pro.local>
2015-12-22 15:29:34 -0700

    fixing date format and getting prod assets working

--- app/models/load.rb
+++ app/models/load.rb
@@ -1,2 +1,12 @@
 class Load < ActiveRecord::Base
+
+  def load_date=(arg)
+    if !arg.nil?
+      if arg.is_a?(String)
+        self[:load_date] = DateTime.strptime(arg, "%m-%d-%Y")
+      else #just push it through
+        self[:load_date] = arg
+      end
+    end
+  end
 end
